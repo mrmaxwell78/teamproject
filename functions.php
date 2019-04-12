85aaa7e1879cf57cb2ac7ad2a3f15cf1f17d7a04
Matthew Maxwell <40220876+mrmaxwell78@users.noreply.github.com>
2019-04-11 20:33:34 -0400

    File to hold relevant functions
    Feel free to add change or edit as needed

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+function displayHeader(){
+?>
+<!DOCTYPE html>
+    <html>
+    <head>
+    <title>Index</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+<?php
+}
+?>
+<?php
+function displayPageFooter() {
+?>
+  </body>
+</html>
+<?php
+}
+?>
