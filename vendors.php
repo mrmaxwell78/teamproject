77579132388381097b12efb19f0a2250d63ad776
ghechavarria <49539243+ghechavarria@users.noreply.github.com>
2019-04-18 20:39:42 -0400

    Update vendors.php

--- vendors.php
+++ vendors.php
@@ -3,5 +3,28 @@
 <head>
 </head>
 <body>
+<?php
+include('InventoryConnection.php');
+$sql = "SELECT * FROM time";
+?>
+<table style="width: 100%; text-align: left;">
+<tr>
+	<th>Inventory ID</th>
+	<th>Name</th>
+	<th>Phone</th>
+	<th>Action<input type='submit' value='Add Time'></th>
+</tr>
+<?php
+try {
+  $rows = $conn->query( $sql );
+  foreach ( $rows as $row ) {
+    echo "<tr><td>" . $row["VendorID"]. "</td><td>" . $row["Name"] . "</td><td>" . $row["Phone"] . "</td><td><input type='submit' value='Update'><input type='submit' value='Delete'></td><tr>";
+  }
+}catch(PDOException $e){
+    echo "<br>Query Failed:" . $e->getMessage();
+}
+echo "</table>";
+$conn = null;
+?>
 </body>
 </html>
