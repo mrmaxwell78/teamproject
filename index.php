572ee3d0ab0454bc9f3e3eeb95f6866f6f8d2b5e
ToussaintG <49769128+ToussaintG@users.noreply.github.com>
2019-04-18 20:53:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,14 @@
 
 <body>
 	<h1>INDEX</h1>
+	
+	<footer>
+		<a href="buildings.php">BUILDINGS</a> |	
+		<a href="rooms.php">ROOMS</a> |	
+		<a href="roomcomputers.php">ROOM COMPUTERS</a> | 
+		<a href="computers.php">COMPUTERS</a> | 
+		<a href="vendors.php">VENDORS</a>
+		
+	</footer>
 </body>
 </html>
