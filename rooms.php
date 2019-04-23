396f5233b75e00d09c107e46fb0385b68423a82c
Matthew Maxwell <40220876+mrmaxwell78@users.noreply.github.com>
2019-04-23 18:29:54 -0400

    Updated rooms.php to use php data from rprocess.php

--- rooms.php
+++ rooms.php
@@ -1,65 +1,44 @@
 <?php
 require_once 'config.php';
 require_once 'functions.php';
+require_once 'rprocess.php';
+
 displayHeader();
+
 try{
     $conn = new PDO($DB, $DBUSER, $DBPASSWORD);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e) {
-    echo "Connection failed!";
-}
-
-function create(){
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $buildingID = $_POST['BuildingID'];
-        $RoomNumber = $_POST['RoomNumber'];
-        $Capacity = $_POST['Capacity'];
-
-        $sql = "INSERT INTO Rooms (BuildingID, RoomNumber, Capacity) 
-        VALUES ('".$buildingID."', '".$RoomNumber."', '".$Capacity."' )";
-        if($conn->query($sql)){
-            echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
-        }
-    }
-}
-
-function update(){
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $buildingID = $_POST['BuildingID'];
-        $RoomNumber = $_POST['RoomNumber'];
-        $Capacity = $_POST['Capacity'];
-
-        $sql = "UPDATE Rooms SET RoomNumber = $RoomNumber Capacity = $Capacity WHERE RoomNumber = $RoomNumber";
-
-        if($conn->query($sql)){
-            echo "<script type= 'text/javascript'>alert('Record Updated Successfully');</script>";
-        }
-}
-
-function del(){
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $RoomNumber = $_POST['RoomNumber'];
-        
-        $sql = "DELETE FROM Rooms WHERE 'RoomNumber' = $RoomNumber";
-
-        if($conn->query($sql)){
-            echo "<script type='text/javascript'>alert('Record Deleted Successfully');</script>";
-        }
-    }
-}
-
+}catch(PDOException $e) {}
 ?>
+<nav>
+    <a href="index.php">Home</a>
+</nav>
+
+<?php
+        $edit = '<a href="movies.php?edit=$row["id"]">Edit</a>';
+        $delete = '<a href="movies.php?delete=$row["id"]">Delete</a>';
+        $result = $conn->query("SELECT * FROM Room");
+        while($row = $result->fetch_assoc()){
+            echo $row['Title'], $edit, $delete;
+            echo $row['Type'], $edit, $delete;
+        }
+    ?>
 
 <form action="" method="POST">
 <label for="BuildingID">Building</label>
-<input type="text" name="BuildingID" id="BuildingID" required><br>
+<input type="text" name="BuildingID" id="BuildingID" value="<?php echo $build ?>" required><br>
 <label for="RoomNumber">Room Number</label>
-<input type="text" name="RoomNumber" id="RoomNumber" required><br>
+<input type="text" name="RoomNumber" id="RoomNumber" value="<?php echo $roomNumner ?>" required><br>
 <label for="Capacity">Capacity</label>
-<input type="text" name="Capacity" id="Capacity" required><br>
-<button onclick="<?php create() ?>" value="Create">
-<button onclick="<?php update() ?>" value="Update">
-<button onclick="<?php del() ?>" value="Delete">
+<input type="text" name="Capacity" id="Capacity" value="<?php echo $capacity ?>" required><br>
+<?php
+    if($update == true){
+       echo '<button type="submit" name="update">Update</button>';
+    } else {
+        echo '<button type="submit" value="insert">Insert</button>';
+    }
+?>
+
 </form>
 
 <?php
---
+++ rprocess.php
@@ -0,0 +1,49 @@
+<?php
+require_once 'config.php';
+try{
+    $conn = new PDO($DB, $DBUSER, $DBPASSWORD);
+} catch(PDOException $e) {}
+
+$id=0;
+$build = "";
+$roomNumber="";
+$capacity = 0;
+$update = false;
+
+if(isset($_POST['insert'])){
+    $build = $_POST['BuildingID'];
+    $roomNumber = $_POST['RoomNumber'];
+    $capacity = $_POST['Capacity'];
+
+    $sql = "INSERT INTO Rooms (BuildingID, Capacity) VALUES ('$build', '$roomNumber', $capacity";
+    $conn->query($sql);
+}
+
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    $sql = "DELETE FROM Rooms WHERE RoomID=$id";
+    $conn->query($sql);
+}
+
+if(isset($_GET['edit'])){
+    $id = $_GET['edit'];
+    $select = "SELECT FROM Rooms WHERE RoomID = $id";
+    $result = $conn->query($select);
+    if(count($result)==1){
+        $row = $result->fetch_array();
+        $title = $row['Title'];
+        $type = $row['type'];
+    
+    }
+}
+
+if(isset($_POST['update'])){
+    $id = $_POST['id'];
+    $title = $_POST['Title'];
+    $type = $_POST['Type'];
+
+    $sql = "UPDATE Rooms SET BuildingID='$build', RoomNumber='$roomNumber', Capacity='$capacity' WHERE RoomID ='$id'";
+    $conn->query($sql);
+}
+
+?>
