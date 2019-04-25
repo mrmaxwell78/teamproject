24a3b180b20ca167ec346834a01686d4552e0a1a
Matthew Maxwell <40220876+mrmaxwell78@users.noreply.github.com>
2019-04-25 19:03:42 -0400

    Add files via upload

--- rooms.php
+++ rooms.php
@@ -13,10 +13,10 @@ try{
 <nav>
     <a href="index.php">Home</a>
 </nav>
-
-<?php
-        $edit = '<a href="movies.php?edit=$row["id"]">Edit</a>';
-        $delete = '<a href="movies.php?delete=$row["id"]">Delete</a>';
+    <input type="submit" value="submit" onclick=document.location.href='roomsform.php'>
+    <?php
+        $edit = '<a href="roomsform.php?edit=$row["id"]">Edit</a>';
+        $delete = '<a href="roomsform.php?delete=$row["id"]">Delete</a>';
         $sql = "SELECT * FROM Rooms";
         foreach($conn->query($sql) as $row){
             echo $row['BuildingID'] . $edit . $delete;
@@ -25,22 +25,7 @@ try{
         }
     ?>
 
-<form action="rprocess.php" method="POST">
-<label for="BuildingID">Building</label>
-<input type="text" name="BuildingID" id="BuildingID" value="<?php echo $build ?>" required><br>
-<label for="RoomNumber">Room Number</label>
-<input type="text" name="RoomNumber" id="RoomNumber" value="<?php echo $roomNumner ?>" required><br>
-<label for="Capacity">Capacity</label>
-<input type="text" name="Capacity" id="Capacity" value="<?php echo $capacity ?>" required><br>
-<?php
-    if($update == true){
-       echo '<button type="submit" name="update">Update</button>';
-    } else {
-        echo '<button type="submit" value="insert">Insert</button>';
-    }
-?>
 
-</form>
 
 <?php
 displayPageFooter();
