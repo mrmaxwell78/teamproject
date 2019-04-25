12538082554823331024e044109ed5cd6cdfea48
ghechavarria <49539243+ghechavarria@users.noreply.github.com>
2019-04-25 19:29:13 -0400

    fixed code of table--working

--- roomcomputers.php
+++ roomcomputers.php
@@ -3,29 +3,31 @@
 <head>
 </head>
 <body>
+<h1>Room Computers Table</h1>
 <?php
   include('InventoryConnection.php'); //DB connection
-  $sql = "SELECT * FROM roomcomoputers;"
+  $sql = "SELECT * FROM roomcomputers;"
  ?>
-  <table style="width:100%; text-align:left;">
-  <tr>
-    <th>Count</th>
-    <th>Room ID</th>
-    <th>Building ID</th>
-    <th>Computer ID</th>
-    <th>Action<input type='submit' value='Add Time'></th>
-  <tr>
+<table style="width: 100%; text-align: left;">
+<tr>
+	<th>Count</th>
+	<th>Room ID</th>
+	<th>Building ID</th>
+	<th>Computer ID</th>
+	<th style="text-align: left;">&nbsp;&nbsp;&nbsp;&nbsp;Action<input type='submit' value='Add Time' style="float: right;"></th>
+</tr>
 <?php
-try{
-  $rows = $conn->query($sql);
-  foreach($rows as $row){
-    echo "<tr><td>" . $row["Count"]."</td><td>" . "</td><td>" . $row["RoomID"] . "</td><td>" . $row["BuildingID"] . "</td><td>" . $row["ComputerID"]. "</td><td><input type='submit' value='Update'><input type='submit' value='Delete'></td><tr>";
+try {
+  $rows = $conn->query( $sql );
+  foreach ( $rows as $row ) {
+    echo "<tr><td>" . $row["Count"]. "</td><td>" . $row["RoomID"]. "</td><td>" . $row["BuildingID"] . "</td><td>" . $row["ComputerID"] . "</td><td><input type='submit' value='Update'> <input type='submit' value='Delete'></td></tr>";
   }
 }catch(PDOException $e){
     echo "<br>Query Failed:" . $e->getMessage();
 }
-echo "</table>";
+echo "</ul>";
 $conn = null;
 ?>
+</table>
 </body>
 </html>
