e90f04ca1c6898ccc238dc03cd0e2c65f12350be
ghechavarria <49539243+ghechavarria@users.noreply.github.com>
2019-04-30 19:38:43 -0400

    added CSS

--- computers.php
+++ computers.php
@@ -6,10 +6,24 @@ require_once 'cprocess.php';
 displayHeader();
 $sql = 'SELECT * FROM Computers';
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Team Project One</title>
+	<link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet">
+	<link href="tableCSS.css" rel="stylesheet">
+</head>
+<body>
+<div id="navbar">
+		<nav>
+		<a href="index.php">HOME</a><a href="buildings.php">BUILDINGS</a><a href="rooms.php">ROOMS</a><a href="roomcomputers.php">ROOM COMPUTERS</a><a href="computers.php" id="selectedpage">COMPUTERS</a><a href="vendors.php">VENDORS</a>
+		</nav>
+		</div>
+		<header><h1>Rooms Table</h1></header>
+		<main>
+<div id="mainContent">
 
-<nav>
-    <a href="index.php">Home</a>
-</nav>
+<div class="tableOverflow">
     <table style="width:100%; text-align:left;">
   <tr>
     <th>Vendor</th>
@@ -30,3 +44,17 @@ try{
 echo "</table>";
 displayPageFooter();
 ?>
+</table>
+</div>
+</div>
+		</main>
+		<footer>
+			<em>
+			<img src="img/group-icon2.png" alt="group icon" height="3%" width="3%">
+			<br>Darly Dubreus, Grace Hechavarria, Mathew Maxwell, 
+			<br>Manuel Pena, Gregory Toussaint
+			<br>copyright &copy; 2019
+			</em>
+		</footer>
+</body>
+</html>
