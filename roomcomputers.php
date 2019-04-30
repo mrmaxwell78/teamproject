b0e27a0292a49d2c0fb616d9f330d19b87d7ebbc
ghechavarria <49539243+ghechavarria@users.noreply.github.com>
2019-04-30 19:45:31 -0400

    added CSS

--- roomcomputers.php
+++ roomcomputers.php
@@ -1,13 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-<h1>Room Computers Table</h1>
 <?php
   include('InventoryConnection.php'); //DB connection
   $sql = "SELECT * FROM roomcomputers;"
  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Team Project One</title>
+	<link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet">
+	<link href="tableCSS.css" rel="stylesheet">
+</head>
+<body>
+<div id="navbar">
+		<nav>
+		<a href="index.php">HOME</a><a href="buildings.php">BUILDINGS</a><a href="rooms.php">ROOMS</a><a href="roomcomputers.php" id="selectedpage">ROOM COMPUTERS</a><a href="computers.php">COMPUTERS</a><a href="vendors.php">VENDORS</a>
+		</nav>
+		</div>
+		<header><h1>Rooms Table</h1></header>
+		<main>
+
+<div id="mainContent">
+
+<div class="tableOverflow">
 <table style="width: 100%; text-align: left;">
 <tr>
 	<th>Count</th>
@@ -28,6 +41,17 @@ try {
 echo "</ul>";
 $conn = null;
 ?>
-</table>
+/table>
+</div>
+</div>
+		</main>
+		<footer>
+			<em>
+			<img src="img/group-icon2.png" alt="group icon" height="3%" width="3%">
+			<br>Darly Dubreus, Grace Hechavarria, Mathew Maxwell, 
+			<br>Manuel Pena, Gregory Toussaint
+			<br>copyright &copy; 2019
+			</em>
+		</footer>
 </body>
 </html>
