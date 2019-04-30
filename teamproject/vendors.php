1a27595db2af4a3700510d3294aa82ae2a100f6a
ghechavarria <49539243+ghechavarria@users.noreply.github.com>
2019-04-30 18:51:38 -0400

    Update vendors.php

--- teamproject/vendors.php
+++ teamproject/vendors.php
@@ -12,7 +12,7 @@ $sql = "SELECT * FROM vendors";
 <body>
 <div id="navbar">
 		<nav>
-		<a href="index.php">HOME</a><a href="buildings.php">BUILDINGS</a><a href="rooms.php" id="selectedpage">ROOMS</a><a href="roomcomputers.php">ROOM COMPUTERS</a><a href="computers.php">COMPUTERS</a><a href="vendors.php">VENDORS</a>
+		<a href="index.php">HOME</a><a href="buildings.php">BUILDINGS</a><a href="rooms.php">ROOMS</a><a href="roomcomputers.php">ROOM COMPUTERS</a><a href="computers.php">COMPUTERS</a><a href="vendors.php" id="selectedpage">VENDORS</a>
 		</nav>
 		</div>
 		<header><h1>Rooms Table</h1></header>
