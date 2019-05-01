ab790e341469b90db3de9cc73c609d7fd44175db
ghechavarria <49539243+ghechavarria@users.noreply.github.com>
2019-04-30 20:24:19 -0400

    added css

--- report.php
+++ report.php
@@ -2,9 +2,18 @@
 <html>
 <head>
 	<title>Team Project One</title>
+	<link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet">
+	<link href="tableCSS.css" rel="stylesheet">
 </head>
-
 <body>
+<div id="navbar">
+	<nav>
+	<a href="index.php">HOME</a><a href="buildings.php">BUILDINGS</a><a href="rooms.php">ROOMS</a><a href="roomcomputers.php">ROOM COMPUTERS</a><a href="computers.php">COMPUTERS</a><a href="vendors.php">VENDORS</a><a href="reports.php" id="selectedpage">REPORTS</a>
+	</nav>
+	</div>
+	<header><h1>Reports</h1></header>
+	<main>
+<div id="mainContent">
 <form action="" method=POST>
     <!-- This is to create something to search the database-->
     <input type="search" name="keyword" placeholder="Search">
@@ -23,5 +32,17 @@
             }
         }
     ?>
+</div>
+		</main>
+		<footer>
+			<em>
+			<img src="img/group-icon2.png" alt="group icon" height="5%" width="3%">
+			<br>Darly Dubreus, Grace Hechavarria, Mathew Maxwell, 
+			<br>Manuel Pena, Gregory Toussaint
+			<br>copyright &copy; 2019
+			</em>
+		</footer>
+</body>
+</html>
 </body>
 </html>
