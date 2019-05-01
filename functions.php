a8cff51c6c29f88b982989310ce5b47fdefd8af3
Matthew Maxwell <40220876+mrmaxwell78@users.noreply.github.com>
2019-04-30 20:57:05 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,21 +1,21 @@
 <?php
 function displayHeader(){
-?>
+echo '
 <!DOCTYPE html>
     <html>
     <head>
     <title>Index</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-    <body>
-<?php
+    <body>';
+   
 }
 ?>
 <?php
 function displayPageFooter() {
-?>
+ echo '
   </body>
-</html>
-<?php
+</html>';
+
 }
 ?>
