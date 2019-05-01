5846a5fbf8624017d0d70ad12eac6f4471557604
ghechavarria <49539243+ghechavarria@users.noreply.github.com>
2019-04-30 20:13:40 -0400

    Darly's CSS

--- buildingform.php
+++ buildingform.php
@@ -1,16 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Team Project One</title>
+	<link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet">
+	<link href="stylesheet/formCSS.css" rel="stylesheet">
+<style>
+body {
+  background-image: url("img/blackbackground.png");
+}
+</style>	
+</head>
+<body>
 <?php
-require_once 'buildings.php';
-require_once 'bprocess.php';
+require_once 'rooms.php';
+require_once 'rprocess.php';
 
 ?>
 
-
-<form action="bprocess.php" method="POST">
-<label for="BuildingNo">Building Number</label>
-<input type="text" name="BuildingNo" id="BuildingNo" value="<?php echo $buildnumber ?>" required><br>
-<label for="BuildingName">Room Number</label>
-<input type="text" name="BuildingName" id="BuildingName" value="<?php echo $buildingname ?>" required><br>
-
+<div id="form"></div>
+<form action="rprocess.php" method="POST">
+<fieldset>
+<legend> 
+Rooms form
+</legend>
+<label for="BuildingID">Building</label>
+<input type="text" name="BuildingID" id="BuildingID" value="<?php echo $build ?>" required><br>
+<label for="RoomNumber">Room Number</label>
+<input type="text" name="RoomNumber" id="RoomNumber" value="<?php echo $roomNumner ?>" required><br>
+<label for="Capacity">Capacity</label>
+<input type="text" name="Capacity" id="Capacity" value="<?php echo $capacity ?>" required><br>
 <?php
     if($update == true){
        echo '<button type="submit" name="update">Update</button>';
@@ -18,4 +36,16 @@ require_once 'bprocess.php';
         echo '<button type="submit" value="insert">Insert</button>';
     }
 ?>
+</fieldset>
 </form>
+   <div id="footer"></div>
+     <footer>
+			<em>
+			<img src="img/group-icon2.png" alt="group icon" height="3%" width="3%">
+			<br>Darly Dubreus, Grace Hechavarria, Mathew Maxwell, 
+			<br>Manuel Pena, Gregory Toussaint
+			<br>copyright &copy; 2019
+			</em>
+		</footer>
+</body>
+</html>
