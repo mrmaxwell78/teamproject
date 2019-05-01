2f6e083119095ffbdc930793a28bcc2968265fa9
ghechavarria <49539243+ghechavarria@users.noreply.github.com>
2019-04-30 20:14:33 -0400

    Darly's CSS

--- computerform.php
+++ computerform.php
@@ -1,9 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Team Project One</title>
+	<link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet">
+	<link href="stylesheet/formCSS.css" rel="stylesheet">
+<style>
+body {
+  background-image: url("img/blackbackground.png");
+}
+</style>
+</head>
+<body>
 <?php
 require_once 'computers.php';
 include('InventoryConnection.php');
 ?>
-
+<div id="form"></div>
 <form action="cprocess.php" method="POST">
+<fieldset>
+<legend> 
+Computer form
+</legend>
 <label for="VendorID">Vendor</label>
 <input type="text" name="VendorID" id="VendorID" value="<?php echo $vendor ?>" required><br>
 <label for="Model">Model</label>
@@ -19,4 +36,15 @@ include('InventoryConnection.php');
         echo '<button type="submit" value="insert">Insert</button>';
     }
 ?>
+</fieldset>
 </form>
+<div id="footer"></div>
+<footer>
+			<em>
+			<img src="img/group-icon2.png" alt="group icon" height="3%" width="3%">
+			<br>Darly Dubreus, Grace Hechavarria, Mathew Maxwell, 
+			<br>Manuel Pena, Gregory Toussaint
+			<br>copyright &copy; 2019
+			</em>
+		</footer>
+</body>
